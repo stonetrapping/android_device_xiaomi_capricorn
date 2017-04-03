97f31b64942a95456fb4b18a943665e7a765892d
hondajohn88 <hondajohn88@gmail.com>
2017-04-02 23:34:21 -0700

    Working on fingerprint and screen brightness. Real progress now.

--- system_prop.mk
+++ system_prop.mk
@@ -82,9 +82,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.qfp=false \
-    persist.qfp.fd_enabled=1 \
-    persist.qfp.home_btn_enabled=1 \
-    persist.qfp.ssc_enable_island=1 \
+    persist.qfp.fd_enabled=0 \
+    persist.qfp.home_btn_enabled=0 \
+    persist.qfp.ssc_enable_island=0 \
     persist.qfp.ssc_fgoff_dil_s1=10 \
     persist.qfp.ssc_fgoff_dil_home=10 \
     persist.qfp.ssc_thresh_s1=0.570 \
@@ -98,11 +98,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.qfp.ssc_basis_s1_off=/persist/qc_senseid/bg_estimation/bg_small1_off_basis.dat \
     persist.qfp.ssc_basis_s2=/persist/qc_senseid/bg_estimation/bg_small2_basis.dat \
     persist.qfp.ssc_basis_s2_off=/persist/qc_senseid/bg_estimation/bg_small2_off_basis.dat \
-    persist.qfp.cbge_active_enabled=1 \
-    persist.qfp.cbge_sleep_enabled=1 \
+    persist.qfp.cbge_active_enabled=0 \
+    persist.qfp.cbge_sleep_enabled=0 \
     persist.qfp.filter_liftfinger=0 \
     persist.qfp.wup_display=0 \
-    persist.qfp.capacitive_enabled=1
+    persist.qfp.capacitive_enabled=0
 
 #property to set fingerpirnt vendor
 PRODUCT_PROPERTY_OVERRIDES += \
