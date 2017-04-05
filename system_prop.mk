36d1129a812223dae11e97119303fe228f7f547f
stonetrapping <tom@stonetrap.info>
2017-04-05 12:33:24 +0200

    Scorpio: Remove QualcommFingerprintReader configs

--- system_prop.mk
+++ system_prop.mk
@@ -82,30 +82,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.qfp=false \
-    persist.qfp.fd_enabled=0 \
-    persist.qfp.home_btn_enabled=0 \
-    persist.qfp.ssc_enable_island=0 \
-    persist.qfp.ssc_fgoff_dil_s1=10 \
-    persist.qfp.ssc_fgoff_dil_home=10 \
-    persist.qfp.ssc_thresh_s1=0.570 \
-    persist.qfp.ssc_thresh_s1_off=0.0 \
-    persist.qfp.ssc_thresh_s1_rub=0.0 \
-    persist.qfp.ssc_thresh_s2=0.430 \
-    persist.qfp.ssc_thresh_s2_off=0.000 \
-    persist.qfp.ssc_thresh_s2_touch=0.330 \
-    persist.qfp.ssc_thresh_s2_lift=0.310 \
-    persist.qfp.ssc_basis_s1=/persist/qc_senseid/bg_estimation/bg_small1_basis.dat \
-    persist.qfp.ssc_basis_s1_off=/persist/qc_senseid/bg_estimation/bg_small1_off_basis.dat \
-    persist.qfp.ssc_basis_s2=/persist/qc_senseid/bg_estimation/bg_small2_basis.dat \
-    persist.qfp.ssc_basis_s2_off=/persist/qc_senseid/bg_estimation/bg_small2_off_basis.dat \
-    persist.qfp.cbge_active_enabled=0 \
-    persist.qfp.cbge_sleep_enabled=0 \
-    persist.qfp.filter_liftfinger=0 \
-    persist.qfp.wup_display=0 \
-    persist.qfp.capacitive_enabled=0
-
-#property to set fingerpirnt vendor
-PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.fingerprint=fpc \
     sys.fpc.tu.disabled=0 \
     ro.qti.sensors.touch_taptap=true
